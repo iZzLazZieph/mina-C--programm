16dadd8a94e59f17ebd7b1c6b24082dd25b26aaf
iZzLazZieph <ikke_johan@icloud.com>
2018-04-29 11:50:13 +0200

    test MBP

---
+++ FibonaccitalV3-Github/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace FibonaccitalV3_Github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen till min Fibonaccital generator!");
+            Console.WriteLine();
+            int x, y, z, v;
+
+        Start:
+
+            Console.WriteLine("Ange maxtal:");
+            Console.WriteLine();
+            try
+            {
+                v = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+            }
+            catch
+            {
+                Console.WriteLine();
+                Console.WriteLine("Felaktig input:");
+                goto Start;
+            }
+            x = 0;
+            y = 1;
+            while (x <= v)
+            {
+                Console.WriteLine(x);
+                z = x + y;
+                x = y;
+                y = z;
+            }
+            Console.WriteLine();
+            goto Start;
+        }
+    }
+}
